7877bef8e09151ab837b393eb2642578365811b0
Thomas Horrobin <thomasroberthorrobin@gmail.com>
2016-09-05 17:00:41 +1200

    added firebase inialization

--- src/index.js
+++ src/index.js
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCYzDDvN-qoKetV8v3HsnmuVe9K3jW6mso",
+  authDomain: "journojs.firebaseapp.com",
+  databaseURL: "https://journojs.firebaseio.com",
+  storageBucket: "",
+};
+firebase.initializeApp(config);
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
