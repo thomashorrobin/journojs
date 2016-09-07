beb1be067159ed33e73bc3ae4012c402b3d4bbd7
Thomas Horrobin <thomasroberthorrobin@gmail.com>
2016-09-07 17:13:34 +1200

    fixed up testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import * as firebase from 'firebase';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCYzDDvN-qoKetV8v3HsnmuVe9K3jW6mso",
+  authDomain: "journojs.firebaseapp.com",
+  databaseURL: "https://journojs.firebaseio.com",
+  storageBucket: "",
+};
+firebase.initializeApp(config);
+
+describe('UI tests', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+  });
 });
+
